5bc7164c1ad825dbf39518956e08df98542ab1e2
ernandorezende-arch <ernandorezende@frwk.com.br>
2025-10-25 19:03:58 +0100

    Understanding data types in Kotlin

--- src/com/ernando/DataTypes.kt
+++ src/com/ernando/DataTypes.kt
@@ -1,15 +1,18 @@
 fun main(args: Array<String>) {
     val myName: String = "Ernando"
-    val myLongNumber: Long = 123_456_789_100_111_123
-    val myFloatNumber: Float = 123.45f
-    val myDoubleNumber: Double = 123.456789
-    val myShortNumber: Short = 123
-    val myByteNumber: Byte = 123
-    val myBoolean: Boolean = true
-    val myChar: Char = 'a'
+    val myByteNumber: Byte = -128 //-127 to 128 8 bits
+    val myShortNumber: Short = 32_767 //-32768 to 32767 16 bits
+    val myIntNumber: Int = 2_147_483_647 //-2147483648 to 2147483647 32 bits
+    val myLongNumber: Long = Long.MAX_VALUE //-9223372036854775808 to 9223372036854775807 64 bits
+    val myFloatNumber: Float = Float.MIN_VALUE // -3.4028235E38 to 3.4028235E38 32 bits
+    val myDoubleNumber: Double = Double.MAX_VALUE // -4.9E-324 to 1.7976931348623157E308 64 bits
+    val myBoolean: Boolean = true // true or false
+    val myChar: Char = 'a' // single character
+
     println("My name is $myName and I'm $myLongNumber years old.")
     println("My float number is $myFloatNumber")
     println("My double number is $myDoubleNumber")
+    println("My int number is $myIntNumber")
     println("My short number is $myShortNumber")
     println("My byte number is $myByteNumber")
     println("My boolean value is $myBoolean")
