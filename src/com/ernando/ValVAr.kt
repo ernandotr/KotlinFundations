8a160d0713731e1976a7c8b91881ea6d8eb0991f
ernandorezende-arch <ernandorezende@frwk.com.br>
2025-10-25 17:52:31 +0100

    First commit

---
+++ src/com/ernando/ValVAr.kt
@@ -0,0 +1,13 @@
+package com.ernando
+
+
+fun main() {
+    val name: String = "Ernando"
+
+    var age: Int = 30
+
+    println("My name is $name and I'm $age years old.")
+    age = age + 12
+
+    println("After twelve years, I'm $age years old.")
+}
