2c5ab9a11394d5425225bb01e7c5cb3cf814bccc
ernandorezende-arch <ernandorezende@frwk.com.br>
2025-10-26 11:32:36 +0000

    Arithmetic operators in Kotlin

---
+++ src/com/ernando/ArithmeticOperator.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val x = 10
+    val y = 9
+    val z = x + y // sum
+    println("Result of $x + $y is $z")
+    val w = z - y // subtract
+    println("Result of $z - $y is $w")
+    val q = w * x // multiply
+    println("Result of $w * $x is $q")
+    val d = q / y // divide
+    println("Result of $q / $y is $d")
+    val  m = q %  y // modulo
+    println("Result of $q % $y is $m")
+
+
+}
